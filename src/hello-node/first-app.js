a1cc2266bdba63b99385ff266d5286a9bac6714f
Mateusz Pomorski <pomorski.mateusz@gmail.com>
2019-01-09 20:27:16 +0000

    First node server

--- src/hello-node/first-app.js
+++ src/hello-node/first-app.js
@@ -1,1 +1,11 @@
-console.log('Hello from node.js!!');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('All good');
+  res.end();
+});
+
+server.listen(3000);
+//   process.exit(); hard exits the node.js event loop
