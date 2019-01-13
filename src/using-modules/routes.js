93cad434190f1c8d14bc0ce1b3973fe5bb84a8cd
Mateusz Pomorski <pomorski.mateusz@gmail.com>
2019-01-13 12:17:23 +0000

    Using node modules system

---
+++ src/using-modules/routes.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    // event fired whenever a new chunk of req is ready to be read
+    req.on('data', chunk => {
+      console.log('New chunk arrived...');
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      console.log('Done parsing incoming data.');
+      const parsedBody = Buffer.concat(body).toString();
+      console.log('Body: ', parsedBody);
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.writeHead(302, { Location: '/' });
+    console.log('Redirecting...');
+    return res.end();
+  }
+};
+
+// global object exposed by node
+module.exports = requestHandler;
+
+module.exports = {
+  handler: requestHandler,
+  someText: 'Message'
+};
+
+module.exports.handler = requestHandler;
+
+// shortcut for nodejs
+exports.handler = requestHandler;
