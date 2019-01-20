54605b5e31bdfe99d1934bd24f2f101c046f230d
Mateusz Pomorski <pomorski.mateusz@gmail.com>
2019-01-20 12:53:15 +0000

    Not found error page added

--- src/express/using-router/app.js
+++ src/express/using-router/app.js
@@ -9,7 +9,12 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // order still matters
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  // send needs to be the last one
+  res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000);
--- src/express/using-router/routes/admin.js
+++ src/express/using-router/routes/admin.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
+router.get('/add-product', (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>'
+    '<form action="/admin/product" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>'
   );
 });
 
