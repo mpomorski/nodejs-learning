321a34fd4f97de146deff55ed78bd585cc68d1f1
Mateusz Pomorski <pomorski.mateusz@gmail.com>
2019-01-26 20:36:33 +0000

    Dummy storage for products

--- src/05-templating/app.js
+++ src/05-templating/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // order still matters
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- src/05-templating/routes/admin.js
+++ src/05-templating/routes/admin.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 });
@@ -10,7 +12,9 @@ router.get('/add-product', (req, res, next) => {
 // when different HTTP methods are used, it is an exact match on path
 router.post('/add-product', (req, res, next) => {
   console.log('req body: ', req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
