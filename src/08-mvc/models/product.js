2a63fc7a1580e70d9094f94920f5bc88d931f388
Mateusz Pomorski <pomorski.mateusz@gmail.com>
2019-02-03 19:16:00 +0000

    Saving products to JSON file.

--- src/08-mvc/models/product.js
+++ src/08-mvc/models/product.js
@@ -1,18 +1,37 @@
 // ES5 constructor function
 // module.exports = function Product() {};
 
-const products = [];
-// esnext gen class
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/path');
+
+const productsFile = path.join(rootDir, 'data', 'products.json');
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    products.push(this);
+    fs.readFile(productsFile, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(productsFile, JSON.stringify(products), err => {
+        if (err) {
+          console.log('Unable to save products', err);
+        }
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    fs.readFile(productsFile, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 };
